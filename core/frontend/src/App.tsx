fee720e8502df9b7d7b389bb0a2b0781b69867b5
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-01-29 17:36:52 -0800

    did i cook

--- core/frontend/src/App.tsx
+++ core/frontend/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <button onClick={() => setCount((count) => count + 15)}>
         count is {count}
       </button>
+      <p>oh to see without my eyes...</p>
     </div>
   );
 }
