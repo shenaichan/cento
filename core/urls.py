dd614890bdca94843eb2cb1fd6752821ceeb91a3
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-01-29 16:42:08 -0800

    main page is react now

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from core.views import index, index_react
+from core.views import index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("react", index_react, name="index_react"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "core/index.html")
-
-
-def index_react(request):
     return render(request, "core/index_react.html")
