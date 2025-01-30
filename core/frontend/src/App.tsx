a42b192e22e06535630003f7375bb25367d5c827
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-01-29 17:07:53 -0800

    update desc text

--- core/frontend/src/App.tsx
+++ core/frontend/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div id="maincontent">
       <h1>welcome to cento maker central!!</h1>
+      <p>if you are curious what a cento is feel free to check out <a href="https://www.poetryfoundation.org/education/glossary/cento">this website</a></p>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
